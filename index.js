7c1e22b4e3c31a6dbfa045efe81e70f7c2ad4bf3
andronix <andryanets@gmail.com>
2017-09-11 08:16:15 +0300

    basic tests

--- index.js
+++ index.js
@@ -8,5 +8,4 @@ const COLS_COUNT = 7;
 const field = new Field(ROWS_COUNT, COLS_COUNT);
 const figures = figuresData.map(lines => new Figure(lines));
 
-field.addFugure(figures[8].rotate(), 0, 0) || console.warn('Unable to add figure');
-console.log(field.lines);
+// start here...
--- test.js
+++ test.js
@@ -1,16 +1,61 @@
+const expect = require('chai').expect;
+
 const figuresData = require('./figuresData');
 const Field = require('./field');
 const Figure = require('./figure');
 
-const ROWS_COUNT = 7;
-const COLS_COUNT = 7;
+const ROWS_COUNT = 5;
+const COLS_COUNT = 5;
 
 const field = new Field(ROWS_COUNT, COLS_COUNT);
-const figures = figuresData.map(lines => new Figure(lines));
 
-const f = figures[5];
-console.log('BEFORE', f.lines);
-f.rotate(); console.log('ROTATE 1', f.lines, f.rotates);
-f.rotate(); console.log('ROTATE 2', f.lines, f.rotates);
-f.rotate(); console.log('ROTATE 3', f.lines, f.rotates);
-f.rotate(); console.log('ROTATE 4', f.lines, f.rotates);
+describe('Puzzle tests', () => {
+
+  beforeEach(() => {
+    field.clear();
+  });
+
+  it('should rotate figure', () => {
+    const f = new Figure([
+      [1, 0],
+      [1, 1],
+      [1, 0],
+      [1, 0],
+    ]);
+
+    f.rotate();
+    expect(f.lines).to.eql([
+      [1, 1, 1, 1],
+      [0, 0, 1, 0]
+    ]);
+    expect(f.rotates).to.eql(1);
+
+    f.rotate();
+    expect(f.lines).to.eql([
+      [0, 1],
+      [0, 1],
+      [1, 1],
+      [0, 1]
+    ]);
+    expect(f.rotates).to.eql(2);
+  });
+
+  it('should place figure on field correctly', () => {
+    const f = new Figure([
+      [1, 0],
+      [1, 1],
+      [1, 0],
+      [1, 0],
+    ]);
+    expect(field.addFugure(f, 1, 3)).to.be.true;
+    expect(field.lines).to.eql([
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0],
+      [0, 0, 0, 1, 1],
+      [0, 0, 0, 1, 0],
+      [0, 0, 0, 1, 0]
+    ]);
+  });
+
+});
+
